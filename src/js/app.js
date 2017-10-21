5d9c7744a22a65995d00dcdccf8d471c98cc16a3
Robert Laws <roblaws@me.com>
2017-10-21 20:56:40 +0300

    html5 javascript api

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log("hello!")
+var img = document.createElement('img')
+img.setAttribute('src', './img/robot_icon.jpg')
+img.setAttribute('class', 'robotImage')
+
+var item = document.getElementsByClassName('one')[0].textContent
+console.log(item[0].textContent)
+
+var thing = document.querySelector('#title')
+console.log(thing.textContent)
+
+var spanItem = document.createElement('span')
+spanItem.innerText = item
+
+var stash = document.getElementById('stash')
+stash.appendChild(spanItem)
+stash.appendChild(img)
+
+console.log(document.querySelectorAll('article li').length) // querySelectorAll -> returns a node list
+var listItems = document.querySelectorAll('article li')
+for(var i = 0; i < listItems.length; i++) {
+  console.log(listItems[i].innerText)
+}
+
+var forEach = Array.prototype.forEach
+forEach.call(listItems, function(item) {
+  console.log(item.innerText + "!")
+})
